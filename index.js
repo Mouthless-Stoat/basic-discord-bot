e35413eb1c17aa0c3b983df459d0addf4fd79147
khanhfg <nphuyanh@gmail.com>
2023-01-21 23:47:04 -0400

    Added all the file

---
+++ index.js
@@ -0,0 +1,21 @@
+const { Client } = require("discord.js")
+const { token, clientId } = require("./config.json")
+
+const client = new Client({
+	intents: [GatewayIntentBits.Guilds],
+})
+
+client.once(Events.ClientReady, () => {
+	console.log("Ready!")
+})
+
+// on commands call
+client.on(Events.InteractionCreate, async (interaction) => {
+	if (!interaction.isChatInputCommand()) return // executing everything but commands interaction
+
+	const { commandName, options } = interaction
+	if (commandName === "test") {
+		await interaction.reply("hello")
+	}
+})
+client.login(token) // login the bot
